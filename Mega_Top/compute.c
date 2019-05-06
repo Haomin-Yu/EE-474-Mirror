125dfbddf147622be9912b870e0fe2510bc29499
Haomin Yu <yuhaomin1996@gmail.com>
2019-05-05 18:38:59 -0700

    partially updated compute.c

--- Mega_Top/compute.c
+++ Mega_Top/compute.c
@@ -16,37 +16,72 @@ double computeSys(unsigned int* systolicRaw);
 double computeDias(unsigned int* diastolicRaw);
 double computePr(unsigned int* bpRaw);
 
-// Global variable
-bool enableCompute = true;
+// Getting global indicators from measure.ino
+extern bool tempRawChanged;
+extern bool sysPressRawChanged;
+extern bool diasPressRawChanged;
+extern bool pulseRateRawChanged;
+
+// Global variables
+// (Indicating whether a new computation has been made)
+bool newTempComputed;
+bool newSysPressComputed;
+bool newDiasPressComputed;
+bool newPulseRateComputed;
 
 // Computes the values for 'tempCorrected', 'sysCorrected',
-// 'diasCorrected', and 'prCorrected'
+// 'diasCorrected', and 'prCorrected' if needed
 void compute(void* Data) {
-  if(enableCompute) {
-  	ComputeDataStruct data = *((ComputeDataStruct*)Data);
-  	*data.tempCorrected      = computeTemp(data.temperatureRaw);
-  	*data.sysPressCorrected  = computeSys(data.systolicPressRaw);
-  	*data.diasPressCorrected = computeDias(data.diastolicPressRaw);
-  	*data.prCorrected        = computePr(data.pulseRateRaw);
-  }
+   ComputeDataStruct data = *((ComputeDataStruct*)Data);
+   if(tempRawChanged) {
+     unsigned short index = (*data.nextTemperatureIndex == 0)? 
+                            7: (*data.nextTemperatureIndex - 1);
+     data.tempCorrectedBuf[index] = computeTemp(data.temperatureRawBuf[*data.nextTemperatureIndex]);
+     tempRawChanged  = false;
+     newTempComputed = true;
+   }
+   if(sysPressRawChanged) {
+     unsigned short index = (*data.nextSysPressIndex == 0)? 
+                            7: (*data.nextSysPressIndex - 1);
+     
+     sysPressRawChanged  = false;
+     newSysPressComputed = true;
+   }
+   if(diasPressRawChanged) {
+     unsigned short index = (*data.nextDiasPressIndex == 0)? 
+                            7: (*data.nextDiasPressIndex - 1);
+     
+     diasPressRawChanged = false;
+     newSysPressComputed = true;
+   }
+   if(pulseRateRawChanged) {
+     unsigned short index = (*data.nextPulseRateIndex == 0)? 
+                            7: (*data.nextPulseRateIndex - 1);
+     data.prCorrectedBuf[index] = computePr(data.pulseRateRawBuf[*data.nextPulseRateIndex]);
+     pulseRateRawChanged = false;
+     newSysPressComputed = true;
+   }
+
+   bloodPressRawBuf
+   bloodPressCorrectedBuf
 }
 
 // Computes the temperature in celsius
-double computeTemp(unsigned int* tempRaw) {
-	return 5 + 0.75*(*tempRaw);
+double computeTemp(unsigned int tempRaw) {
+	return 5 + 0.75*tempRaw;
 }
 
 // Computes the systolic pressure in mmHg
-double computeSys(unsigned int* systolicRaw) {
-	return 9 + 2*(*systolicRaw);
+double computeSys(unsigned int systolicRaw) {
+	return 9 + 2*systolicRaw;
 }
 
 // Computes the diastolic pressure in mmHg
-double computeDias(unsigned int* diastolicRaw) {
-  return 6 + 1.5*(*diastolicRaw);
+double computeDias(unsigned int diastolicRaw) {
+  return 6 + 1.5*diastolicRaw;
 }
 
 // Computes the pulse rate in beats per minute
-double computePr(unsigned int* bpRaw) {
-	return 8 + 3*(*bpRaw);
+double computePr(unsigned int bpRaw) {
+	return 8 + 3*bpRaw;
 }
