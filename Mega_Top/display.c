29079c4186438bc622f8122307ccd8037dbe5dd5
Haomin Yu <yuhaomin1996@gmail.com>
2019-05-05 17:42:18 -0700

    display.c checkpoint

--- Mega_Top/display.c
+++ Mega_Top/display.c
@@ -2,7 +2,7 @@
  * Function name: display
  * Function inputs: inputs the rawstructsas well as the string.h fle
  * Function outputs: outputs the data to be displayed
- * Function description: it is used to set the actual calcultated values to be displayed.
+ * Function description: it is used to set the actual calcultated values to be displayed
  * Author: Haomin Yu
  */
 #include "rawStructs.h"
@@ -11,17 +11,16 @@
 // Function Prototypes
 void display(void* Data);
 // Global parameter
-bool measurementChanged = true;
-bool enableDisplay      = true;
+bool tempChanged      = true;
+bool sysPressChanged  = true;
+bool diasPressChanged = true;
+bool pulseRateChanged = true;
 
-// Display of the system
+// Display function of the system
 void display(void* Data) {
-  if(enableDisplay) {
-    DisplayDataStruct data = *((DisplayDataStruct*)Data);
-    *data.tempCorrected = *data.tempCorrected;
-    *data.sysPressCorrected = *data.sysPressCorrected;
-    *data.diasCorrected = *data.diasCorrected;
-    *data.prCorrected = *data.prCorrected;
-  }
-  measurementChanged = enableDisplay;
+   DisplayDataStruct data = *((DisplayDataStruct*)Data);
+   *data.tempCorrected = *data.tempCorrected;
+   *data.sysPressCorrected = *data.sysPressCorrected;
+   *data.diasCorrected = *data.diasCorrected;
+   *data.prCorrected = *data.prCorrected;
 }
