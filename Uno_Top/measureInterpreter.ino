3251d74a137a9b597dbc303b2878142b526d0d1f
Haomin Yu <yuhaomin1996@gmail.com>
2019-05-09 18:46:27 -0700

    Fixed typos

---
+++ Uno_Top/measureInterpreter.ino
@@ -0,0 +1,36 @@
+/* 
+ * (Type shit here)
+ * 
+ * Author: Nathan Ness
+ */
+// Pin assignments
+static const int INTERRUPT     = 2;
+static const int PULSE_DIGITAL = 13;
+static const int PULSE_IN      = 4;
+// Class variable
+static unsigned long COUNTER = 0;
+ 
+unsigned long pulseInterupt(){
+  COUNTER++;
+}
+
+
+
+
+// Simulates raw input of pulse rate
+// (This function is deprecated as of Project 3)
+static const unsigned int PULSE_THRESHOLD = 5;
+void pulseRateSimulator(unsigned int* prValuePointer) {
+  unsigned long startTime = millis();
+  COUNTER = 0;
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT), pulseInterupt, FALLING);
+  while (millis() - startTime < 6000) {
+    if(analogRead(PULSE_IN) < PULSE_THRESHOLD) {
+      digitalWrite(PULSE_DIGITAL, LOW);
+    } else {
+      digitalWrite(PULSE_DIGITAL, HIGH);
+    }
+  }
+  detachInterrupt(INTERRUPT);
+  *prValuePointer = COUNTER * 10;
+}
